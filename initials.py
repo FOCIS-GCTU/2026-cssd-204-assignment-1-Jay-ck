a614d632c6203c90605d043acff31287c7c65c7a
Jay-ck <2425402719@live.gctu.edu.gh>
2026-05-30 04:06:20 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in big stylized block letters.
+# Assignment Number: 1
+#
+# Name: Amoah Jason Nyarko
+# STUDENT ID: 2425402719
+# Email: 2425402719@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Amoah Jason Nyarko, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print a blank line, then the small initials, then a blank line,
+    # then the big block letters for A, J, and N, then a final blank line.
 
+    print()
+    print("...AJN")
+    print()
+    print(".......AAAA............JJJJJJJJJJJJ........N.........NN.....")
+    print("......AA..AA.....................JJ........NN........NN.....")
+    print(".....AA....AA....................JJ........NNN.......NN.....")
+    print("....AA......AA...................JJ........NN.N......NN.....")
+    print("...AA........AA..................JJ........NN..N.....NN.....")
+    print("...AAAAAAAAAAAA..................JJ........NN...N....NN.....")
+    print("...AA........AA........J.........JJ........NN....N...NN.....")
+    print("...AA........AA........JJ........JJ........NN.....N..NN.....")
+    print("...AA........AA...**....JJJJJJJJJ.....**...NN......N.NN...**")
+    print("...AA........AA...**......JJJJJJ......**...NN.......NNN...**")
+    print()
 
 
 main()
